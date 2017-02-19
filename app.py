63c58d7576dafcdadb8dd4290874c0a47e0b5b6a
Anton Prokhorov <betrayer11@gmail.com>
2017-02-19 15:01:31 +0700

    Add index route

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ import asyncio
 
 from aiohttp import web
 
+from projects.views import index
+
 loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
 app: web.Application = web.Application()
+
+app.router.add_get('/', index)
 
 web.run_app(app, host='localhost', port=8000)
---
+++ projects/views.py
@@ -0,0 +1,14 @@
+from aiohttp import web
+
+
+async def index(request: web.Request) -> web.Response:
+    """Index view
+
+    Args:
+        request (web.Request): Request instance
+
+    Returns:
+        web.Response: Response instance
+
+    """
+    return web.Response(text='Hello world')
