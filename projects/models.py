62437b1d7e1249b57e5e33371948109d3e29f4d6
Anton Prokhorov <betrayer11@gmail.com>
2017-02-19 20:00:01 +0700

    Remove index view logic and add project model

---
+++ projects/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Project(Base):
+    """A project model
+
+    Attributes:
+        id (int): ID
+        name (str): Title
+    """
+    __tablename__ = 'projects'
+
+    id: int = Column(Integer, primary_key=True)
+    title: str = Column(String)
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,4 @@
-from typing import Dict, List
-
-import peewee_async
 from aiohttp import web
-
-from .models import Project, database
 
 
 async def index(request: web.Request) -> web.Response:
@@ -16,11 +11,5 @@ async def index(request: web.Request) -> web.Response:
         web.Response: Response instance
 
     """
-    objects: peewee_async.Manager = peewee_async.Manager(database)
-    project_list: List[Dict] = []
-    results: List[Project] = await objects.execute(Project.select())
 
-    for project in results:
-        project_list.append({'id': project.id, 'title': project.title})
-
-    return web.json_response(project_list)
+    return web.json_response({'hello': 'world'})
