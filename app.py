1855efb9101e70db2fd1cae219200e2788027db0
Anton Prokhorov <betrayer11@gmail.com>
2017-02-19 19:46:07 +0700

    Add init and clean up of db

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ import asyncio
 
 from aiohttp import web
 
+from db import close_pg, init_pg
 from projects.views import index
 
 loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
 app: web.Application = web.Application()
+
+app.on_startup.append(init_pg)
+app.on_cleanup.append(close_pg)
 
 app.router.add_get('/', index)
 
---
+++ db.py
@@ -0,0 +1,30 @@
+from aiohttp import web
+from aiopg.sa import create_engine
+
+
+async def init_pg(app: web.Application):
+    """Initialize async pg engine
+
+    Args:
+        app (web.Application): Application instance
+
+    """
+    engine = await create_engine(
+        database='async-lab',
+        user='async-lab',
+        password='async-lab',
+        host='postgres',
+        loop=app.loop
+    )
+    app['db'] = engine
+
+
+async def close_pg(app: web.Application):
+    """Close async PG engine
+
+    Args:
+        app (web.Application): Application instance
+
+    """
+    app['db'].close()
+    await app['db'].wait_closed()
