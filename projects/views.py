295298aae62edea7cba70754f017cb5efcdc6150
Anton Prokhorov <betrayer11@gmail.com>
2017-02-19 15:52:27 +0700

    Add list of projects returning to index view

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,9 @@
+from typing import Dict, List
+
+import peewee_async
 from aiohttp import web
+
+from .models import Project, database
 
 
 async def index(request: web.Request) -> web.Response:
@@ -11,4 +16,11 @@ async def index(request: web.Request) -> web.Response:
         web.Response: Response instance
 
     """
-    return web.Response(text='Hello world')
+    objects: peewee_async.Manager = peewee_async.Manager(database)
+    project_list: List[Dict] = []
+    results: List[Project] = await objects.execute(Project.select())
+
+    for project in results:
+        project_list.append({'id': project.id, 'title': project.title})
+
+    return web.json_response(project_list)
